1f08a6e447adee6cb86833e01ae2c738787cf6b3
Yan Feng <frankie.yanfeng@gmail.com>
2024-01-06 01:44:15 +0800

    Provision EC2 with public and private subnets.

--- exercises/exercise2/main.tf
+++ exercises/exercise2/main.tf
@@ -217,7 +217,7 @@ resource "aws_launch_template" "launchtemplate1" {
 
   image_id               = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
-  key_name               = var.key_name
+  #key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.webserver.id]
 
   tag_specifications {
